f981a5619bcc614bcd12cd04d823f666593248a5
nboutin <boutwork@gmail.com>
2019-03-01 09:15:30 +0100

    gen.sh accept args

--- gen.sh
+++ gen.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
 
-pdflatex -output-directory=output nicolas_boutin_cv.tex
-pdflatex -output-directory=output nicolas_boutin_cv_full.tex
+if [ $1 = "cv" ] || [ -z $1 ]; then
+	pdflatex -output-directory=output nicolas_boutin_cv.tex
+elif [ $1 = "full" ]; then
+	pdflatex -output-directory=output nicolas_boutin_cv_full.tex
+elif [ $1 = "all" ]; then
+	pdflatex -output-directory=output nicolas_boutin_cv.tex
+	pdflatex -output-directory=output nicolas_boutin_cv_full.tex
+fi
